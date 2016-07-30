f33619fbf0da324a025055812571d80d92aaf74d
David Ruiz <druiz2412@gmail.com>
2016-07-30 12:59:31 -0400

    namespaced js setup

--- public/script/homepage.js
+++ public/script/homepage.js
@@ -1,15 +1,30 @@
+
+
 $(()=>{
 
   // nonsense
-  $('#name').typeIt({
-       speed: 500,
-       autoStart: false
-  })
-  .tiType('dr.')
-  .tiPause(2000)
-  .tiDelete(2)
-  .tiSettings({speed: 100})
-  .tiType('avid ')
-  .tiPause(500)
-  .tiType('ruiz.')
+  HOME.name.write();
+
+
+
+});
+
+var HOME = HOME || {};
+
+$.extend(true, HOME, {
+  name: {
+      write: function() {
+      $('#name').typeIt({
+           speed: 500,
+           autoStart: false
+      })
+      .tiType('dr.')
+      .tiPause(2000)
+      .tiDelete(2)
+      .tiSettings({speed: 100})
+      .tiType('avid ')
+      .tiPause(500)
+      .tiType('ruiz.');
+    }
+  }
 });
